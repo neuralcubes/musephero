f0876dfd173841ea41128733d6ccd124f98d6e04
Ramón Argüello <monchote@gmail.com>
2016-04-10 12:32:11 +0100

    Add Sphero connectivity and testing code

--- app/src/main/java/uk/co/neuralcubes/neuralates/MainActivity.java
+++ app/src/main/java/uk/co/neuralcubes/neuralates/MainActivity.java
@@ -1,19 +1,34 @@
 package uk.co.neuralcubes.neuralates;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.orbotix.ConvenienceRobot;
 import com.orbotix.DualStackDiscoveryAgent;
+import com.orbotix.common.DiscoveryException;
 import com.orbotix.common.Robot;
 import com.orbotix.common.RobotChangedStateListener;
+import com.orbotix.le.RobotLE;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements RobotChangedStateListener {
+
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 42;
+
+    private ConvenienceRobot mSphero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +41,55 @@ public class MainActivity extends AppCompatActivity implements RobotChangedState
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, R.string.searching_for_spheros, Snackbar.LENGTH_LONG).show();
             }
         });
 
-        DualStackDiscoveryAgent.getInstance().addRobotStateListener( this );
+        DualStackDiscoveryAgent.getInstance().addRobotStateListener(this);
+        requestLocationPermission();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M
+                || checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            startDiscovery();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        //If the DiscoveryAgent is in discovery mode, stop it.
+        if(DualStackDiscoveryAgent.getInstance().isDiscovering()) {
+            DualStackDiscoveryAgent.getInstance().stopDiscovery();
+        }
+
+        //If a robot is connected to the device, disconnect it
+        if(mSphero != null) {
+            mSphero.disconnect();
+            mSphero = null;
+        }
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        DualStackDiscoveryAgent.getInstance().addRobotStateListener(null);
+    }
+
+    private void startDiscovery() {
+        //If the DiscoveryAgent is not already looking for robots, start discovery.
+        if(!DualStackDiscoveryAgent.getInstance().isDiscovering()) {
+            try {
+                DualStackDiscoveryAgent.getInstance().startDiscovery(this);
+            } catch (DiscoveryException e) {
+                Log.e("Sphero", "DiscoveryException: " + e.getMessage());
+            }
+        }
     }
 
     @Override
@@ -58,6 +116,74 @@ public class MainActivity extends AppCompatActivity implements RobotChangedState
 
     @Override
     public void handleRobotChangedState(Robot robot, RobotChangedStateNotificationType robotChangedStateNotificationType) {
+        switch(robotChangedStateNotificationType) {
+            case Online: {
+                //If robot uses Bluetooth LE, Developer Mode can be turned on.
+                //This turns off DOS protection. This generally isn't required.
+                if( robot instanceof RobotLE) {
+                    ((RobotLE)robot).setDeveloperMode(true);
+                }
 
+                //Save the robot as a ConvenienceRobot for additional utility methods
+                mSphero = new ConvenienceRobot(robot);
+
+                //Start blinking the robot's LED
+                blink( false );
+                break;
+            }
+        }
+    }
+
+    //Turn the robot LED on or off every two seconds
+    private void blink( final boolean lit ) {
+        if( mSphero == null )
+            return;
+
+        if( lit ) {
+            mSphero.setLed( 0.0f, 0.0f, 0.0f );
+        } else {
+            mSphero.setLed( 0.0f, 0.0f, 1.0f );
+        }
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                blink(!lit);
+            }
+        }, 2000);
+    }
+
+    private void requestLocationPermission() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int hasLocationPermission = checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION);
+            if(hasLocationPermission != PackageManager.PERMISSION_GRANTED) {
+                Log.e("Sphero", "Location permission has not already been granted");
+                List<String> permissions = new ArrayList<String>();
+                permissions.add( Manifest.permission.ACCESS_COARSE_LOCATION);
+                requestPermissions(permissions.toArray(new String[permissions.size()]), REQUEST_CODE_LOCATION_PERMISSION);
+            } else {
+                Log.d("Sphero", "Location permission already granted");
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_LOCATION_PERMISSION: {
+                for(int i = 0; i < permissions.length; i++ ) {
+                    if(grantResults[i] == PackageManager.PERMISSION_GRANTED ) {
+                        startDiscovery();
+                        Log.d("Permissions", "Permission Granted: " + permissions[i]);
+                    } else if( grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                        Log.d("Permissions", "Permission Denied: " + permissions[i]);
+                    }
+                }
+            }
+            break;
+            default: {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
     }
 }
