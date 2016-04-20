54abeeaab24585c79662fd8b93a10cdc7be76851
j.manzanaresartolazabal@gmail.com <j.manzanaresartolazabal@gmail.com>
2016-04-20 22:19:14 +0100

    Curly braces added

--- app/src/main/java/uk/co/neuralcubes/neuralates/ControlFragment.java
+++ app/src/main/java/uk/co/neuralcubes/neuralates/ControlFragment.java
@@ -139,7 +139,9 @@ public class ControlFragment extends Fragment implements SpheroEventListener, Ad
             setEnabledStateForViews(mMuseActions, true);
 
             if (mSphero.isPresent()) {
-                if (mController.isPresent()) mController.get().unlink();
+                if (mController.isPresent()) {
+                    mController.get().unlink();
+                }
                 mController = Optional.of(new RobotController(mSphero.get(), mBus));
             }
         }
