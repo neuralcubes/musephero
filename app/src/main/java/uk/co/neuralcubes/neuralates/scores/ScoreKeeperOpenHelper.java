e0953a3b44a1d6b07e63515262a2de08d3cd1f2f
jmanart <j.manzanaresartolazabal@gmail.com>
2016-04-25 09:37:42 +0100

    Issue/27 (#54)
    * SQLite helper for score persistence.
    
    * Define fields to save accumulated score and start/stop times.Save Time for Start and Stop.
    
    * Encapsulate score data for player
    
    * Save icon
    
    * Add PlayerName string
    
    * add code to show score and save it
    
    * Save score to SQLite db. Helper class.
    
    * Add display name edition, save button and display of score
    
    * Text size for the score
    
    * Add elements to the layout:
     - Input for player name
     - Save button
     - Score display
    
    * Fix layout
    
    * Set ColorMap initial value, deleted in rebase
    
    * Move Bar outside of layout.
    Restore text size of score.

---
+++ app/src/main/java/uk/co/neuralcubes/neuralates/scores/ScoreKeeperOpenHelper.java
@@ -0,0 +1,43 @@
+package uk.co.neuralcubes.neuralates.scores;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+/**
+ * Created by jmanart on 22/04/2016.
+ */
+public class ScoreKeeperOpenHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "ScoreBoard.db";
+
+    public static abstract class ScoreEntry implements BaseColumns {
+        public static final String TABLE_NAME = "score";
+        public static final String COLUMN_NAME_ENTRY_ID = "user";
+        public static final String COLUMN_NAME_SCORE = "score";
+        public static final String COLUMN_NAME_ACCUMULATED = "accumulated";
+        public static final String COLUMN_NAME_DURATION = "duration";
+        public static final String COLUMN_NAME_TIMESTAMP = "timestamp";
+    }
+
+    private static final String SQL_CREATE_DATABASE = "CREATE TABLE " + ScoreEntry.TABLE_NAME +
+            " ( " + ScoreEntry._ID + " INTEGER PRIMARY KEY, " +
+            ScoreEntry.COLUMN_NAME_ENTRY_ID + " TEXT, " +
+            ScoreEntry.COLUMN_NAME_SCORE + " FLOAT, " +
+            ScoreEntry.COLUMN_NAME_ACCUMULATED + " FLOAT, " +
+            ScoreEntry.COLUMN_NAME_DURATION + " INTEGER, " +
+            ScoreEntry.COLUMN_NAME_TIMESTAMP + " INTEGER " +
+            ")";
+
+    public ScoreKeeperOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_DATABASE);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int from, int to) {
+    }
+}
